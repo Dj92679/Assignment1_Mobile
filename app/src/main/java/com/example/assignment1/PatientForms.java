aa3843357e357de7585acf2ae243f287738eeb63
Dj92679 <dhailes021@gmail.com>
2022-05-11 22:26:13 +1200

    Tried to fix navigation errors was unsuccessful

--- app/src/main/java/com/example/assignment1/PatientForms.java
+++ app/src/main/java/com/example/assignment1/PatientForms.java
@@ -10,27 +10,29 @@ import android.widget.EditText;
 
 public class PatientForms extends AppCompatActivity {
 
-    EditText Incident, PrevMedHis, ReligionAdmit, GPAdmit;
-    Button FormSubmit;
+    Button formSubmit;
+    EditText GP, religion, prevMedHis, incidentReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_forms);
 
-        Incident = findViewById(R.id.Incident);
-        PrevMedHis = findViewById(R.id.PrevMedHis);
-        ReligionAdmit = findViewById(R.id.ReligionAdmit);
-        GPAdmit = findViewById(R.id.GPAdmit);
+       prevMedHis = findViewById(R.id.PrevMedHis);
+       GP = findViewById(R.id.GPAdmit);
+       religion = findViewById(R.id.ReligionAdmit);
+       incidentReport = findViewById(R.id.Incident);
 
-        FormSubmit = findViewById(R.id.PatientSettings);
+       formSubmit = findViewById(R.id.FormSubmitBtn);
 
-        FormSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(PatientForms.this, Settings.class);
-                startActivity(intent);
-            }
-        });
+       formSubmit.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(PatientForms.this, PatientHomeScreen.class);
+               startActivity(intent);
+           }
+       });
+
+
     }
 }
